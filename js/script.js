20b3648a17656896bfff0bd1cea9eb831cba2fa9
Mystic Kitty <djabranton@gmail.com>
2021-06-02 12:08:59 -0700

    Started js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // alert("Doc is loaded")
+
+    $('.add-queue').click(function () {
+        document.location = 'edit.html';
+    });
+
+    $('.leave').click(function () {
+        document.location = 'https://services.promiles.com/';
+    });
+
+
+});
