f1e5c60679c8d1a042e592142b3ed9806d8b3c99
Mystic Kitty <djabranton@gmail.com>
2021-06-02 13:17:44 -0700

    Touched up.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
     // alert("Doc is loaded")
 
+    //Link to Edit page
     $('.add-queue').click(function () {
         document.location = 'edit.html';
     });
 
+
+
+    //Exit Page
     $('.leave').click(function () {
         document.location = 'https://services.promiles.com/';
     });
 
-    $("select").change(function () {
-        $(this).find("option:selected").each(function () {
-            var optionValue = $(this).attr("value");
-            if (optionValue) {
-                $(".big-permits").not("." + optionValue).hide();
-                $("." + optionValue).show();
-            } else {
-                $(".big-permits").hide();
-            }
-        });
-    }).change();
+    //Back to Index
+    $('.index').click(function () {
+        document.location = 'index.html';
+    });
+
+
+    //Toggle Between column content -- Just the begining.  Can be done as a feature taking all layouts in to consideration
+    // $("select").change(function () {
+    //     $(this).find("option:selected").each(function () {
+    //         var optionValue = $(this).attr("value");
+    //         if (optionValue) {
+    //             $(".big-permits").not("." + optionValue).hide();
+    //             $("." + optionValue).show();
+    //         } else {
+    //             $(".big-permits").hide();
+    //         }
+    //     });
+    // }).change();
 
 
 });
